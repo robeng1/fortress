96a548eb54fe42a676c8ec843b5a8c12e48bb3dd
Romeo Obeng <obengromeo1@gmail.com>
2021-10-11 16:26:52 +0000

    changed font to poppins

--- src/css/tailwind.config.js
+++ src/css/tailwind.config.js
@@ -175,7 +175,7 @@ module.exports = {
       },
       fontFamily: {
         sans: [
-          'Inter',
+          'Poppins',
           'ui-sans-serif',
           'system-ui',
           '-apple-system',
@@ -184,7 +184,7 @@ module.exports = {
           'Roboto',
           'Helvetica Neue',
           'Arial',
-          'Inter',
+          'Poppins',
           'sans-serif',
           'Apple Color Emoji',
           'Segoe UI Emoji',
@@ -192,7 +192,7 @@ module.exports = {
           'Noto Color Emoji',
         ],
         body: [
-          'Inter',
+          'Poppins',
           'ui-sans-serif',
           'system-ui',
           '-apple-system',
@@ -201,7 +201,7 @@ module.exports = {
           'Roboto',
           'Helvetica Neue',
           'Arial',
-          'Inter',
+          'Poppins',
           'sans-serif',
           'Apple Color Emoji',
           'Segoe UI Emoji',
