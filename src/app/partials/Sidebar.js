96611eb529073912dc4a4037607b20b0611665d3
Romeo Obeng <obengromeo1@gmail.com>
2021-11-17 09:49:26 +0000

    enabled discount on sidebar

--- src/app/partials/Sidebar.js
+++ src/app/partials/Sidebar.js
@@ -485,7 +485,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                   }}
                 </SidebarLinkGroup> */}
                 <li
-                  className={`px-3 py-2 rounded-sm mb-0.5 hidden md:block last:mb-0 ${
+                  className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${
                     pathname === '/discounts' &&
                     'w-full bg-white rounded-lg shadow ease-out transition-transform transition-medium'
                   }`}
