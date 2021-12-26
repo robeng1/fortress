3d8e8c86c6636fb3028248eb0e5ef24ede02fca0
Romeo Obeng <obengromeo1@gmail.com>
2021-12-26 05:09:41 +0000

    added copy right info

--- src/app/components/common/footer.tsx
+++ src/app/components/common/footer.tsx
@@ -5,19 +5,19 @@ export function Footer() {
     <footer className="py-4 px-5 self-start">
       <nav className="flex space-x-2">
         <a
-          href="#0"
+          href="https://reoplex.com"
           className="font-semibold text-sm text-gray-500 hover:text-dark"
         >
           © Reoplex
         </a>
         <a
-          href="#0"
+          href="https://reoplex.com/contact"
           className="font-semibold text-sm text-gray-500 hover:text-dark"
         >
           Contact
         </a>
         <a
-          href="#0"
+          href="https://reoplex.com/terms"
           className="font-semibold text-sm text-gray-500 hover:text-dark"
         >
           Terms
