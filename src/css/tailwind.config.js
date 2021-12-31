f0b0706745fce55647429b5655f3813dde1fda6b
Romeo Obeng <obengromeo1@gmail.com>
2021-12-31 15:11:14 +0000

    minor fixes

--- src/css/tailwind.config.js
+++ src/css/tailwind.config.js
@@ -14,6 +14,7 @@ function withOpacity(variableName) {
 module.exports = {
   mode: 'jit',
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class',
   theme: {
     extend: {
       boxShadow: {
