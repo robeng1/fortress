e5ea5a37af713c91e4679a45a8f8019ba9522528
Romeo Obeng <obengromeo1@gmail.com>
2022-05-11 12:13:27 +0000

    revamped transactions ui

--- src/partials/balance/transaction-item.tsx
+++ src/partials/balance/transaction-item.tsx
@@ -12,17 +12,17 @@ const TransactionItem: React.FC<TransactionItemItemProps> = ({ txn }) => {
     >
       <div className="md:flex justify-between items-center space-y-4 md:space-y-0 space-x-2">
         {/* Left side */}
-        <div className="flex items-start space-x-3 md:space-x-4">
+        <div className="flex items-start space-x-2 md:space-x-4">
           <div className="w-9 h-9 shrink-0 mt-1">
-            <div className="w-9 h-9 rounded-full">{txn.txn_kind.charAt(0)}</div>
+            <div className="w-8 h-8 relative flex justify-center items-center rounded-full bg-gray-500 text-xl text-white uppercase">{txn.txn_kind.charAt(0)}</div>
           </div>
           <div>
             <div className="text-sm">{txn.description}</div>
           </div>
         </div>
         {/* Right side */}
-        <div className="flex items-center space-x-4 pl-10 md:pl-0">
-          <div className="text-sm text-slate-500 italic whitespace-nowrap">{new Date(txn.created_at).toDateString()}</div>
+        <div className="flex items-center space-x-2 md:space-x-4 pl-3 md:pl-0">
+          <div className="text-sm text-slate-500 italic whitespace-nowrap">{new Date(txn.created_at).toLocaleDateString()}</div>
           {txn.txn_kind && (
             <div
               className={`text-xs inline-flex font-medium rounded-full text-center px-2.5 py-1 ${txn.txn_kind === 'CREDIT' ? 'bg-amber-100 text-amber-600' : 'bg-emerald-100 text-emerald-600'
