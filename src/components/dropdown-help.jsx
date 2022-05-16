4db29c333cb83f614517179887f9972b3ed2d825
Romeo Obeng <obengromeo1@gmail.com>
2022-05-16 06:39:30 +0000

    support stuff

--- src/components/dropdown-help.jsx
+++ src/components/dropdown-help.jsx
@@ -84,9 +84,9 @@ function DropdownHelp({ align }) {
           </div>
           <ul>
             <li>
-              <Link
+              <a
                 className="font-medium text-sm text-purple-500 hover:text-purple-600 flex items-center py-1 px-3"
-                to="#0"
+                href="mailto:romeo@reoplex.com"
                 onClick={() => setDropdownOpen(!dropdownOpen)}
               >
                 <svg
@@ -96,12 +96,12 @@ function DropdownHelp({ align }) {
                   <path d="M10.5 0h-9A1.5 1.5 0 000 1.5v9A1.5 1.5 0 001.5 12h9a1.5 1.5 0 001.5-1.5v-9A1.5 1.5 0 0010.5 0zM10 7L8.207 5.207l-3 3-1.414-1.414 3-3L5 2h5v5z" />
                 </svg>
                 <span>Support Site</span>
-              </Link>
+              </a>
             </li>
             <li>
-              <Link
+              <a
                 className="font-medium text-sm text-purple-500 hover:text-purple-600 flex items-center py-1 px-3"
-                to="#0"
+                href="mailto:romeo@reoplex.com"
                 onClick={() => setDropdownOpen(!dropdownOpen)}
               >
                 <svg
@@ -111,7 +111,22 @@ function DropdownHelp({ align }) {
                   <path d="M11.854.146a.5.5 0 00-.525-.116l-11 4a.5.5 0 00-.015.934l4.8 1.921 1.921 4.8A.5.5 0 007.5 12h.008a.5.5 0 00.462-.329l4-11a.5.5 0 00-.116-.525z" />
                 </svg>
                 <span>Contact us</span>
-              </Link>
+              </a>
+            </li>
+            <li>
+              <a
+                className="font-medium text-sm text-purple-500 hover:text-purple-600 flex items-center py-1 px-3"
+                href="mailto:romeo@reoplex.com"
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+              >
+                <svg
+                  className="w-3 h-3 fill-current text-purple-300 flex-shrink-0 mr-2"
+                  viewBox="0 0 12 12"
+                >
+                  <path d="M11.854.146a.5.5 0 00-.525-.116l-11 4a.5.5 0 00-.015.934l4.8 1.921 1.921 4.8A.5.5 0 007.5 12h.008a.5.5 0 00.462-.329l4-11a.5.5 0 00-.116-.525z" />
+                </svg>
+                <span>Call/WhatsApp: +233246493078</span>
+              </a>
             </li>
           </ul>
         </div>
--- src/partials/header/Help.jsx
+++ src/partials/header/Help.jsx
@@ -96,7 +96,7 @@ function Help() {
             <li>
               <Link
                 className="font-medium text-sm text-purple-500 hover:text-purple-600 flex items-center py-1 px-3"
-                to="#0"
+                to="mailto:romeo@reoplex.com"
                 onClick={() => setDropdownOpen(!dropdownOpen)}
               >
                 <svg
@@ -111,7 +111,7 @@ function Help() {
             <li>
               <Link
                 className="font-medium text-sm text-purple-500 hover:text-purple-600 flex items-center py-1 px-3"
-                to="#0"
+                to="mailto:romeo@reoplex.com"
                 onClick={() => setDropdownOpen(!dropdownOpen)}
               >
                 <svg
@@ -121,6 +121,21 @@ function Help() {
                   <path d="M11.854.146a.5.5 0 00-.525-.116l-11 4a.5.5 0 00-.015.934l4.8 1.921 1.921 4.8A.5.5 0 007.5 12h.008a.5.5 0 00.462-.329l4-11a.5.5 0 00-.116-.525z" />
                 </svg>
                 <span>Contact us</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="font-medium text-sm text-purple-500 hover:text-purple-600 flex items-center py-1 px-3"
+                to="mailto:romeo@reoplex.com"
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+              >
+                <svg
+                  className="w-3 h-3 fill-current text-purple-300 flex-shrink-0 mr-2"
+                  viewBox="0 0 12 12"
+                >
+                  <path d="M11.854.146a.5.5 0 00-.525-.116l-11 4a.5.5 0 00-.015.934l4.8 1.921 1.921 4.8A.5.5 0 007.5 12h.008a.5.5 0 00.462-.329l4-11a.5.5 0 00-.116-.525z" />
+                </svg>
+                <span>Call/WhatsApp: &plus;233246493078</span>
               </Link>
             </li>
           </ul>
